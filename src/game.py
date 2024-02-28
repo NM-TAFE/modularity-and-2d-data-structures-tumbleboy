b2f7d84c0526db224eee093a4c77c932e49c74dd
tumbleboy <jarredosborne123@gmail.com>
2024-02-28 11:49:52 +0800

    Update print board function and add move function
    Allows the program to make dynamically sized boards and print appropriately, game moves don't function properly yet

--- src/game.py
+++ src/game.py
@@ -1,8 +1,16 @@
 import board
 
-board = board.Board(3, 3)
 
-for row in board.grid:
-    print(row)
+class Game:
+    def __init__(self):
+        self.board = board.Board(5, 5)
+        self.players = ["X", "O"]
+
+    def play(self):
+        while True:
+            self.board.print_board()
+            self.board.make_move(self.players)
 
 
+game = Game()
+game.play()
